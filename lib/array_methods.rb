ec30b39e31d4b95ec81edb78c7ada5bcd63546b9
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-10 20:54:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,21 +3,26 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-  array.sort
+  sorted_array = array.sort
+  sorted_array
 end
 
 def using_reverse(array)
-  array.reverse
+  reversed_array = array.reverse
+  reversed_array
 end
 
 def using_first(array)
-
+  first_element = array.first
+  first_element
 end
 
 def using_last(array)
-
+  last_element = array.last 
+  last_element
 end
 
 def using_size(array)
-
+  array_size = array.size 
+  array_size
 end
